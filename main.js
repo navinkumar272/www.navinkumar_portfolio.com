e4f5c1f4e5b8ec383f720c7e9be133218d46e8d8
navinkumar272 <snavinkumar.tvm@gmail.com>
2024-05-17 22:50:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Civil Engineer", "Master of Technology in Geoinformatics"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
